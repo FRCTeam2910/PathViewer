3072cf271641240b015654adb5cb4ef44deb0344
Quinn Pfeifer <72775766+QuinnP2910@users.noreply.github.com>
2022-11-23 21:04:49 -0800

    Fix ControlPoints not being across from each other and not moving with a FieldPoint when dragged (#8)

--- src/main/java/org/frcteam2910/pathviewer/FieldPrimaryControlPoint.java
+++ src/main/java/org/frcteam2910/pathviewer/FieldPrimaryControlPoint.java
@@ -1,28 +1,51 @@
 package org.frcteam2910.pathviewer;
 
 import javafx.beans.property.DoubleProperty;
+import javafx.scene.input.MouseEvent;
+import org.frcteam2910.common.math.Vector2;
+
+import java.util.ArrayList;
 
 public class FieldPrimaryControlPoint extends FieldPoint {
     public static final int RADIUS = 3;
     public static final String STYLE_CLASS_NAME = "field-primary-control-point";
+    public final ArrayList<FieldSecondaryControlPoint> connectedSecondaryControlPoints = new ArrayList<>();
 
     public FieldPrimaryControlPoint(DoubleProperty x, DoubleProperty y) {
         super(x, y, RADIUS);
 
         getStyleClass().add(STYLE_CLASS_NAME);
-        setOnMouseDragged(mouseEvent -> {
-            setCenterX(mouseEvent.getX());
-            setCenterY(mouseEvent.getY());
-        });
+        setOnMouseDragged(this::dragHandler);
     }
 
     public FieldPrimaryControlPoint(double x, double y) {
         super(x, y, RADIUS);
 
         getStyleClass().add(STYLE_CLASS_NAME);
-        setOnMouseDragged(mouseEvent -> {
-            setCenterX(mouseEvent.getX());
-            setCenterY(mouseEvent.getY());
-        });
+        setOnMouseDragged(this::dragHandler);
+    }
+
+    public void dragHandler(MouseEvent mouseEvent) {
+        Vector2 firstConnectedPointDistance = null;
+        Vector2 secondConnectedPointDistance = null;
+        
+        if(connectedSecondaryControlPoints.size() > 0) {
+            firstConnectedPointDistance = connectedSecondaryControlPoints.get(0).getCenter().subtract(getCenter());
+        }
+        if(connectedSecondaryControlPoints.size() > 1) {
+            secondConnectedPointDistance = connectedSecondaryControlPoints.get(1).getCenter().subtract(getCenter());
+        }
+        setCenterX(mouseEvent.getX());
+        setCenterY(mouseEvent.getY());
+        if(connectedSecondaryControlPoints.size() > 0) {
+            connectedSecondaryControlPoints.get(0).setCenter(getCenter().add(firstConnectedPointDistance));
+        }
+        if(connectedSecondaryControlPoints.size() > 1) {
+            connectedSecondaryControlPoints.get(1).setCenter(getCenter().add(secondConnectedPointDistance));
+        }
+    }
+
+    public void setConnectedSecondaryControlPoint(FieldSecondaryControlPoint connectedSecondaryPoint) {
+        connectedSecondaryControlPoints.add(connectedSecondaryPoint);
     }
 }
--- src/main/java/org/frcteam2910/pathviewer/FieldSecondaryControlPoint.java
+++ src/main/java/org/frcteam2910/pathviewer/FieldSecondaryControlPoint.java
@@ -1,28 +1,47 @@
 package org.frcteam2910.pathviewer;
 
 import javafx.beans.property.DoubleProperty;
+import javafx.scene.input.MouseEvent;
+import org.frcteam2910.common.math.Vector2;
 
 public class FieldSecondaryControlPoint extends FieldPoint {
     public static final int RADIUS = 3;
     public static final String STYLE_CLASS_NAME = "field-secondary-control-point";
+    public final FieldPrimaryControlPoint connectedPrimaryControlPoint;
+    public FieldSecondaryControlPoint connectedSecondaryControlPoint = null;
 
-    public FieldSecondaryControlPoint(DoubleProperty x, DoubleProperty y) {
+    public FieldSecondaryControlPoint(DoubleProperty x, DoubleProperty y, FieldPrimaryControlPoint connectedPrimaryControlPoint) {
         super(x, y, RADIUS);
 
+        this.connectedPrimaryControlPoint = connectedPrimaryControlPoint;
+
         getStyleClass().add(STYLE_CLASS_NAME);
-        setOnMouseDragged(mouseEvent -> {
-            setCenterX(mouseEvent.getX());
-            setCenterY(mouseEvent.getY());
-        });
+        setOnMouseDragged(this::dragHandler);
     }
 
-    public FieldSecondaryControlPoint(double x, double y) {
+    public FieldSecondaryControlPoint(double x, double y, FieldPrimaryControlPoint connectedPrimaryControlPoint) {
         super(x, y, RADIUS);
 
+        this.connectedPrimaryControlPoint = connectedPrimaryControlPoint;
+
         getStyleClass().add(STYLE_CLASS_NAME);
-        setOnMouseDragged(mouseEvent -> {
-            setCenterX(mouseEvent.getX());
-            setCenterY(mouseEvent.getY());
-        });
+        setOnMouseDragged(this::dragHandler);
+    }
+
+    private void dragHandler(MouseEvent mouseEvent) {
+        setCenterX(mouseEvent.getX());
+        setCenterY(mouseEvent.getY());
+        if(connectedSecondaryControlPoint != null) {
+            Vector2 distanceFromThisToCenter = getCenter().subtract(connectedPrimaryControlPoint.getCenter());
+            Vector2 distanceFromConnectedToCenter = connectedSecondaryControlPoint.getCenter().subtract(connectedPrimaryControlPoint.getCenter());
+
+            distanceFromConnectedToCenter = distanceFromThisToCenter.normal().scale(-distanceFromConnectedToCenter.length);
+
+            connectedSecondaryControlPoint.setCenter(connectedPrimaryControlPoint.getCenter().add(distanceFromConnectedToCenter));
+        }
+    }
+
+    public void setConnectedSecondaryControlPoint(FieldSecondaryControlPoint connectedSecondaryControlPoint){
+        this.connectedSecondaryControlPoint = connectedSecondaryControlPoint;
     }
 }
